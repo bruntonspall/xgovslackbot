07c7bfa4df228b25f612daac94c1f4447da79d38
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2017-04-04 12:57:28 +0100

    Initial commit

---
+++ slack_bot.js
@@ -0,0 +1,219 @@
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var botkitStoragePostgres = require('botkit-storage-postgres');
+var Botkit = require('botkit');
+var os = require('os');
+var request = require('request');
+
+const port = process.env.PORT || 3000;
+
+
+var controller = Botkit.slackbot({
+    debug: true,
+    storage: botkitStoragePostgres({
+      host: process.env.DATABASE_URL,
+      user: 'botkit',
+      password: 'botkit',
+      database: 'botkit'
+    })
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello '+message.user);
+        }
+    });
+});
+
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var name = message.match[1];
+    controller.storage.users.get(message.user, function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user, function(err, id) {
+            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
+
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Your name is ' + user.name);
+        } else {
+            bot.startConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.say('I do not know your name yet!');
+                    convo.ask('What should I call you?', function(response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [
+                            {
+                                pattern: 'yes',
+                                callback: function(response, convo) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: 'no',
+                                callback: function(response, convo) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: function(response, convo) {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    }, {'key': 'nickname'}); // store the results in a field called nickname
+
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message, 'OK! I will update my dossier...');
+
+                            controller.storage.users.get(message.user, function(err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+                                controller.storage.users.save(user, function(err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+
+
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+controller.hears(['announce #([a-z]*) (.*)'],
+  'direct_message, direct_mention', function(bot, message) {
+    var channel = message.match[1];
+    var msgtext = message.match[2];
+    if (channel === "bots") {
+      bot.say(
+        {
+          "text": "@announce "+msgtext+" (via )",
+          "channel": channel
+        }
+      )
+    } else {
+      bot.reply(message, "If I were working, I'd say '@announce "+msgtext+"' to "+channel);
+    }
+  }
+)
+
+
+controller.hears(['invite (.*@.*)'],
+  'direct_message, direct_mention', function(bot, message) {
+    var res = inviteUser(message.match[1]);
+    if (res === "OK") {
+      bot.reply(message, "Invite sent, tell them to check their email");
+    } else {
+      if (res === "invalid_email") {
+        bot.reply(message, "The email is not valid");
+      }
+      else if (res === "invalid_auth") {
+        bot.reply(message, "The Governor doesn't have the rights to do that")
+      }
+    }
+  }
+)
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention,mention', function(bot, message) {
+
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
+
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+    });
+
+function inviteUser(email) {
+  request.post({
+        url: 'https://ukgovernmentdigital.slack.com/api/users.admin.invite',
+        form: {
+          email: email,
+          token: process.env.apitoken,
+          set_active: true
+        }
+      }, function(err, httpResponse, body) {
+        // body looks like:
+        //   {"ok":true}
+        //       or
+        //   {"ok":false,"error":"already_invited"}
+        if (err) { return res.send('Error:' + err); }
+        body = JSON.parse(body);
+        if (body.ok) {
+          return "OK";
+        } else {
+          return body.error;
+        }
+      });
+}
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
